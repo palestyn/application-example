e03f17b858479cab8c5a6038f238d24f3ca15fcd
mehdi@venturedive.com <mehdi@venturedive.com>
2019-08-16 19:30:32 +0500

    Example updated to show how application started event can be consumed

--- src/main/java/com/example/application/ExampleApplication.java
+++ src/main/java/com/example/application/ExampleApplication.java
@@ -3,11 +3,16 @@ package com.example.application;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Produces;
 import javax.sql.DataSource;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ext.Provider;
+
+import org.palestyn.events.ApplicationStarted;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.example.resources.PersonResource;
 import com.zaxxer.hikari.HikariConfig;
@@ -17,11 +22,16 @@ import com.zaxxer.hikari.HikariDataSource;
 // use .setContextPath("/example") in Launcher
 public class ExampleApplication extends Application {
 	
+	Logger logger = LoggerFactory.getLogger(ExampleApplication.class);
 	
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> set = new HashSet<Class<?>>();
 		set.add(PersonResource.class);
 		return set;
+	}
+	
+	public void onApplicationStarted(@Observes ApplicationStarted e) {
+		logger.info("Application started..");
 	}
 }
